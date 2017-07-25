088d2440ce01d02d02bb7cbf910de1de8919b4d5
venudayyam <dayyam.venu@gmail.com>
2017-07-25 16:26:28 +0530

    Update smartinput.c

--- smartinput.c
+++ smartinput.c
@@ -237,8 +237,8 @@ int fget_line(char *s, int len, FILE *stream)
 
     if (strchr(s,'\n') == NULL)
     {
-    	fscanf(stream,"%*[^\n]");
-    	fgetc(stream);
+        int ch;
+        while((ch = fgetc(stream)) != '\n' && ch != EOF);
     }   	
 
     return SUCCESS;
